1f66e0aee48a0fc89e7ec69515d64fecba2a639f
xujintao <xujintao@126.com>
2019-08-02 03:24:35 +0800

    package dll

--- client/dll/dll.go
+++ client/dll/dll.go
@@ -3,238 +3,226 @@ package dll
 // advapi32.dll
 // 这里定义所有用到的API指针变量，36个
 // 最后一个DWORD为0
-type advapi32 struct {
-	CryptGetUserKey func()
-	RegCLoseKey     func()
-	RegSetValueExA  func()
+type advapi32 interface {
+	CryptGetUserKey()
+	RegCLoseKey()
+	RegSetValueExA()
 	// ...
-	RegQueryValueExW func()
-	end              uintptr
+	RegQueryValueExW()
 }
 
 // crypt32.dll
 // 这里定义所有用到的API指针变量，33个
 // 最后一个DWORD为0
-type crypt32 struct {
-	CertFreeCertificateChainEngine func()
-	CertFreeCertificateChain       func()
-	CertGetIntendedKeyUsage        func()
+type crypt32 interface {
+	CertFreeCertificateChainEngine()
+	CertFreeCertificateChain()
+	CertGetIntendedKeyUsage()
 	// ...
-	CertCreateCertificateChainEngine func()
-	end                              uintptr
+	CertCreateCertificateChainEngine()
 }
 
 // dsound.dll, /c/windows/syswow64/dsound.dll
 // 这里定义所有用到的API指针变量，2个
 // 最后一个DWORD为0
-type dsound struct {
-	DirectSoundEnumerateA func()
-	DirectSoundCreate     func()
-	end                   uintptr
+type dsound interface {
+	DirectSoundEnumerateA()
+	DirectSoundCreate()
 }
 
 // gdi32.dll, /c/windows/syswow64/gdi32.dll
 // 这里定义所有用到的API指针变量，20个
 // 最后一个DWORD为0
-type gdi32 struct {
-	CreateCompatibleDC func()
-	CreateDIBSection   func()
-	DeleteDC           func()
+type gdi32 interface {
+	CreateCompatibleDC()
+	CreateDIBSection()
+	DeleteDC()
 	// ...
-	GetTextMetricsW func()
-	CreateFontA     func()
-	end             uintptr
+	GetTextMetricsW()
+	CreateFontA()
 }
 
 // glu32.dll, /c/windows/syswow64/glu32.dll
 // 这里定义所有用到的API指针变量，3个
 // 最后一个DWORD为0
-type glu32 struct {
-	gluPerspective func()
-	gluLookAt      func()
-	gluOrtho2D     func()
-	end            uintptr
+type glu32 interface {
+	gluPerspective()
+	gluLookAt()
+	gluOrtho2D()
 }
 
 // imm32.dll, /c/windows/syswow64/imm32.dll
 // 这里定义所有用到的API指针变量，22个
 // 最后一个DWORD为0
-type imm32 struct {
-	ImmSetConversionStatus   func()
-	ImmGetCompositionWindow  func()
-	ImmGetCompositionStringA func()
-	ImmIsUIMessageA          func()
+type imm32 interface {
+	ImmSetConversionStatus()
+	ImmGetCompositionWindow()
+	ImmGetCompositionStringA()
+	ImmIsUIMessageA()
 	// ...
-	ImmReleaseContext func()
-	end               uintptr
+	ImmReleaseContext()
 }
 
 // iphlpapi.dll, /c/windows/syswow64/iphlpapi.dll
 // 这里定义所有用到的API指针变量，1个
 // 最后一个DWORD为0
-type iphlpapi struct {
-	GetAdaptersInfo func()
-	end             uintptr
+type iphlpapi interface {
+	GetAdaptersInfo()
 }
 
 // kernel32.dll, /c/windows/syswow64/kernel32.dll
 // 这里定义所有用到的API指针变量，219个
 // 最后一个DWORD为0
 // IAT多了第一个VirtualQueryEx
-type kernel32 struct {
-	GlobalLock      func()
-	CreateFileA     func()
-	GetCommandLineA func()
-	CloseHandle     func()
+type kernel32 interface {
+	GlobalLock()
+	CreateFileA()
+	GetCommandLineA()
+	CloseHandle()
 	// ...
-	GetTickCOunt func()
-	end          uintptr
+	GetTickCOunt()
 }
 
 // oleaut32.dll, /c/windows/syswow64/oleaut32.dll
 // 这里定义所有用到的API指针变量，6个
 // 最后一个DWORD为0
-type oleaut32 struct {
-	SysStringLen          func()
-	VariantInit           func()
-	VariantClear          func()
-	SysAllocStringByteLen func()
-	SysAllocString        func()
-	SysFreeString         func()
-	end                   uintptr
+type oleaut32 interface {
+	SysStringLen()
+	VariantInit()
+	VariantClear()
+	SysAllocStringByteLen()
+	SysAllocString()
+	SysFreeString()
 }
 
 // opengl32.dll, /c/windows/syswow64/opengl32.dll
 // 这里定义所有用到的API指针变量，75个
 // 最后一个DWORD为0
-type opengl32 struct {
-	glScissor         func()
-	glStencilMask     func()
-	glTexCoordPointer func()
+type opengl32 interface {
+	glScissor()
+	glStencilMask()
+	glTexCoordPointer()
 	// ...
-	wglGetProcAddress func()
-	end               uintptr
+	wglGetProcAddress()
 }
 
 // shell32.dll, /c/windows/syswow64/shell32.dll
 // 这里定义所有用到的API指针变量，1个
 // 最后一个DWORD为0
-type shell32 struct {
-	ShellExecuteA func()
-	end           uintptr
+type shell32 interface {
+	ShellExecuteA()
 }
 
 // sh1wapi.dll, /c/windows/syswow64/sh1wapi.dll
 // 这里定义所有用到的API指针变量，1个
 // 最后一个DWORD为0
-type sh1wapi struct {
-	PathFileExistsA func()
-	end             uintptr
+type sh1wapi interface {
+	PathFileExistsA()
 }
 
 // user32.dll, /c/windows/syswow64/user32.dll
 // 这里定义所有用到的API指针变量，100个
 // 最后一个DWORD为0
-type user32 struct {
-	FindWindowA  func()
-	IntersetRect func()
-	wsprintfA    func()
+type user32 interface {
+	FindWindowA()
+	IntersetRect()
+	wsprintfA()
 	// ...
-	SetWindowLongW func()
-	ReleaseDC      func()
-	end            uintptr
+	SetWindowLongW()
+	ReleaseDC()
 }
 
 // version.dll, /c/windows/syswow64/version.dll
 // 这里定义所有用到的API指针变量，3个
 // 最后一个DWORD为0
-type version struct {
-	VerQueryValueA          func()
-	GetFileVersionInfoSizeA func()
-	GetFileVersionInfoA     func()
+type version interface {
+	VerQueryValueA()
+	GetFileVersionInfoSizeA()
+	GetFileVersionInfoA()
 }
 
 // wininet.dll, /c/windows/syswow64/wininet.dll
 // 这里定义所有用到的API指针变量，15个
 // 最后一个DWORD为0
-type wininet struct {
-	InternetOpenA    func()
-	InternetConnectA func()
+type wininet interface {
+	InternetOpenA()
+	InternetConnectA()
 	// ...
-	InternetOpenUrlA func()
-	InternetConnectW func()
-	end              uintptr
+	InternetOpenUrlA()
+	InternetConnectW()
 }
 
 // winmm.dll, /c/windows/syswow64/winmm.dll
 // 这里定义所有用到的API指针变量，12个
 // 最后一个DWORD为0
-type winmm struct {
-	timeGetDevCaps  func()
-	timeBeginPeriod func()
-	timeKillEvent   func()
+type winmm interface {
+	timeGetDevCaps()
+	timeBeginPeriod()
+	timeKillEvent()
 	// ...
-	timeEndPeriod func()
-	end           uintptr
+	timeEndPeriod()
 }
 
 // ws2_32.dll, /c/windows/syswow64/ws2_32.dll
 // 这里定义所有用到的API指针变量，35个
 // 最后一个DWORD为0
-type ws2_32 struct {
-	ntohl       func()
-	getnameinfo func()
-	gethostname func()
-	ioctlsocket func()
+type ws2_32 interface {
+	ntohl()
+	getnameinfo()
+	gethostname()
+	ioctlsocket()
 	// ...
-	WSAStartup func()
-	end        uintptr
+	WSAStartup()
 }
 
 // dbghelp.dll, /c/windows/syswow64/dbghelp.dll
 // debghelp.dll, /c/mu/dbghelp.dll 这个和系统的有什么区别？
 // 这里定义所有用到的API指针变量，7个
 // 最后一个DWORD为0
-type dbghelp struct {
-	SymGetLineFromAddr64 func()
-	SymFromAddr          func()
-	StackWalk64          func()
-	SymInitialize        func()
-	SymSetOptions        func()
-	MiniDumpWriteDump    func()
-	SymCleanup           func()
-	end                  uintptr
+type dbghelp interface {
+	SymGetLineFromAddr64()
+	SymFromAddr()
+	StackWalk64()
+	SymInitialize()
+	SymSetOptions()
+	MiniDumpWriteDump()
+	SymCleanup()
 }
 
 // ole32.dll, /c/windows/syswow64/ole32.dll
 // 这里定义所有用到的API指针变量，4个
 // 最后一个DWORD为0
-type ole32 struct {
-	CoTaskMemFree    func()
-	CoCreateInstance func()
-	CoUninitialize   func()
-	CoInitialize     func()
-	end              uintptr
+type ole32 interface {
+	CoTaskMemFree()
+	CoCreateInstance()
+	CoUninitialize()
+	CoInitialize()
 }
 
 // urlmon.dll, /c/windows/syswow64/urlmon.dll
 // 这里定义所有用到的API指针变量，2个
 // 最后一个DWORD为0
-type urlmon struct {
-	URLDownloadToFileA func()
-	URLDownloadToFileW func()
-	end                uintptr
+type urlmon interface {
+	URLDownloadToFileA()
+	URLDownloadToFileW()
 }
 
 // wzaudio.dll, /c/mu/wzAudio.dll
 // 这里定义所有用到的API指针变量，6个
 // 最后一个DWORD为0
-type wzaudio struct {
-	wzAudioPlay                 func()
-	wzAudioGetStreamOffsetRange func()
-	wzAudioDestroy              func()
-	wzAudioOption               func()
-	wzAudioCreate               func()
-	wzAudioStop                 func()
-	end                         uintptr
+type wzaudio interface {
+	wzAudioPlay()
+	wzAudioGetStreamOffsetRange()
+	wzAudioDestroy()
+	wzAudioOption()
+	wzAudioCreate()
+	wzAudioStop()
+}
+
+type dll struct {
+	Name string
+	Path string
+	i    interface{}
+	Num  int
+	Desc string
 }
