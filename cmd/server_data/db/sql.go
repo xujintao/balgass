265329aeeffbc03b7715343baede7985852f21c0
xujintao <xujintao@126.com>
2019-12-14 15:49:59 +0800

    cmd/server_data: add sql

--- cmd/server_data/db/sql.go
+++ cmd/server_data/db/sql.go
@@ -6,22 +6,72 @@ func Lookup(name string) string {
 }
 
 var index = map[string]string{
-	"account-find-passwd":    accountFindPasswd,
-	"ban_machine-find-count": banMachineFindCount,
+	"account-find-passwd":         accountFindPasswd,
+	"ban_machine-find-count":      banMachineFindCount,
+	"account_join_history-insert": accountJoinHistoryInsert,
+	"account_state-insert":        accountStateInsert,
+	"account_state-update":        accountStateUpdate,
 }
 
 var accountFindPasswd = `
 SELECT
- memb__pwd
+ memb_guid
+,memb__pwd
 ,bloc_code
 FROM MEMB_INFO
 WHERE
-memb___id = ?
+memb___id = @p1
 `
 var banMachineFindCount = `
 SELECT
  count(*)
 FROM IGC_MachineID_Banned
 WHERE
-HWID = ?
+HWID = @p1
+`
+
+var accountJoinHistoryInsert = `
+INSERT INTO ConnectionHistory (
+ AccountID
+,ServerName
+,IP
+,Date
+,State
+,HWID
+)
+VALUES (
+ :AccountID
+,:ServerName
+,:IP
+,:Date
+,:State
+,:HWID
+)
+`
+
+var accountStateInsert = `
+INSERT INTO MEMB_STAT (
+ memb___id
+,ConnectStat
+,ServerName
+,IP
+,ConnectTM
+)
+VALUES (
+ :memb___id
+,:ConnectStat
+,:ServerName
+,:IP
+,:ConnectTM
+)
+`
+
+var accountStateUpdate = `
+UPDATE MEMB_STAT
+SET
+ ConnectStat = :ConnectStat
+,ServerName = :ServerName
+,IP = :IP
+,ConnectTM = :ConnectTM
+WHERE memb___id = ?
 `
