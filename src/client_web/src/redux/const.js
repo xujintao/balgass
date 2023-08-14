69dbc768beca371a4f90f3daa46b721165d45ec1
xujintao <xujintao@126.com>
2023-08-15 00:32:09 +0800

    client_web/refactor: remove

--- src/client_web/src/redux/const.js
+++
@@ -1,9 +0,0 @@
-export const SEARCH_BEGIN = "search-begin";
-export const SEARCH_LOADING = "search-loading";
-export const SEARCH_USERS = "search-users";
-export const SEARCH_ERR = "search-err";
-
-export const MODAL_JOIN = "modal-join";
-export const MODAL_LOGIN = "modal-login";
-export const MODAL_PASSWORD = "modal-password";
-export const MODAL_CLOSE = "modal-close";
