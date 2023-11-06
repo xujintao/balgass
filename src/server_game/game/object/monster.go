910bf91fb6d516ce97b9b12e0f276b702103ff95
xujintao <xujintao@126.com>
2023-11-06 20:35:33 +0800

    server_game/fix: fixed npc erratic

--- src/server_game/game/object/monster.go
+++ src/server_game/game/object/monster.go
@@ -248,6 +248,7 @@ func (m *Monster) spawnPosition() {
 	m.StartX, m.StartY = m.randPosition(m.MapNumber, m.spawnStartX, m.spawnStartY, m.spawnEndX, m.spawnEndY)
 	maps.MapManager.SetMapAttrStand(m.MapNumber, m.StartX, m.StartY)
 	m.X, m.Y = m.StartX, m.StartY
+	m.TX, m.TY = m.X, m.Y
 	m.Dir = m.spawnDir
 	if m.Dir < 0 {
 		m.Dir = rand.Intn(8)
