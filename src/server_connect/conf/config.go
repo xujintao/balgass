fd17efd76f3c468c71994b90e4872331b3cf741a
xujintao <xujintao@126.com>
2023-11-08 17:02:08 +0800

    server_connect/chore: add err log to config module failed

--- src/server_connect/conf/config.go
+++ src/server_connect/conf/config.go
@@ -16,7 +16,7 @@ func INI(dir, file, section string, v interface{}) {
 	log.Printf("Load %s:%s", file, section)
 	cfg, err := ini.Load(file)
 	if err != nil {
-		log.Fatalf("Failed to load %s", file)
+		log.Fatalln(err)
 	}
 	cfg.Section(section).MapTo(v)
 }
@@ -26,7 +26,7 @@ func XML(dir, file string, v interface{}) {
 	log.Printf("Load %s", file)
 	buf, err := ioutil.ReadFile(file)
 	if err != nil {
-		log.Fatalf("Failed to read %s, %v", file, err)
+		log.Fatalln(err)
 	}
 	if err := xml.Unmarshal(buf, v); err != nil {
 		log.Fatalf("Failed to unmarshal %s, %v", file, err)
