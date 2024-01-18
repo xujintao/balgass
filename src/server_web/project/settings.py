2688d6d2b04adc76db6b25b13b9cd771b092da90
xujintao <xujintao@126.com>
2024-01-18 17:36:56 +0800

    server_web/feat: use postgresql

--- src/server_web/project/settings.py
+++ src/server_web/project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django",
+        "USER": "root",
+        "PASSWORD": "1234",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
