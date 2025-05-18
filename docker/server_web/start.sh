28b8a07887db3fd524fe4cc6b6c7471a4b95677d
xujintao <xujintao@126.com>
2025-05-18 17:30:35 -0600

    balgass/chore: add .env file for server_web

--- docker/server_web/start.sh
+++ docker/server_web/start.sh
@@ -2,9 +2,12 @@
 
 TAG=${1:-latest}
 
+ENV_FILE=~/balgass/config/server_web/.env
+
 docker run -d \
 --name server_web \
 --restart always \
+--env-file $ENV_FILE \
 -e TZ=Asia/Shanghai \
 -p 8000:8000 \
 xujintao/server_web:$TAG
