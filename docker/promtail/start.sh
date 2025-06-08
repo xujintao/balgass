6fca71faeee2dce190a305b864fca4a5dffbdd63
xujintao <xujintao@126.com>
2025-06-08 01:52:25 -0600

    docker/feat: collect logs for nginx and mail

--- docker/promtail/start.sh
+++ docker/promtail/start.sh
@@ -1,5 +1,7 @@
 DOCKER_DIR=~/balgass/docker
 PROMTAIL_DIR=$DOCKER_DIR/promtail
+NGINX_FAIL2BAN_LOG_DIR=$DOCKER_DIR/nginx-fail2ban/logs
+MAIL_LOG_DIR=$DOCKER_DIR/mail/logs
 
 docker run \
 --restart always \
@@ -8,6 +10,7 @@ docker run \
 -e LANG=C.UTF-8 \
 -e TZ=UTC \
 -v $PROMTAIL_DIR/config.yml:/etc/promtail/config.yml:ro \
--v $DOCKER_DIR/nginx-fail2ban/logs:/var/log/nginx-fail2ban:ro \
+-v $NGINX_FAIL2BAN_LOG_DIR:/var/log/nginx-fail2ban:ro \
+-v $MAIL_LOG_DIR:/var/log/mail:ro \
 -p 9080:9080 \
 grafana/promtail:3.5.1
