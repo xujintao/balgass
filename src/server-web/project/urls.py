63ff178cf03e134ba6af273a8b5d368d3bf007df
xujintao <xujintao@126.com>
2025-08-01 23:57:11 -0600

    server-web/feat: add password reset logic

--- src/server-web/project/urls.py
+++ src/server-web/project/urls.py
@@ -25,6 +25,24 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("login/", views.CustomLoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path(
+        "password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
     path("signup/", views.signup, name="signup"),
     path(
         "resend-verification-email/",
