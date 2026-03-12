e6bcd1f37a3246ebc58dfd85f7d95cc58fb19f44
xujintao <xujintao@126.com>
2026-03-12 12:36:23 -0600

    docker/fix: update Loki container volume mapping to use named volume

--- docker/loki/start.sh
+++ docker/loki/start.sh
@@ -2,12 +2,14 @@ DOCKER_DIR=~/balgass/docker
 LOKI_DIR=$DOCKER_DIR/loki
 LOKI_DATA=$LOKI_DIR/data
 
+docker volume create loki_data
+
 docker run \
 --restart always \
 -d \
 --name loki \
 -e LANG=C.UTF-8 \
 -e TZ=UTC \
--v $LOKI_DATA:/tmp/loki \
+-v loki_data:/loki \
 -p 3100:3100 \
 grafana/loki:3.5.1
